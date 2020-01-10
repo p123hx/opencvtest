7d29cf4909433aadddedd1ce2b7144af39dcf47c
hongxi <p123hx@gami.com>
2020-01-10 12:11:56 +0800

    out.txt

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 #include <ctime>
 #include <cstdlib>
 #include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/imgproc.hpp>
 #include <string>
 using namespace std;
 using namespace cv;
